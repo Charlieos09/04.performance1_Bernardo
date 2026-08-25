4cf636fac049adb478650edf815a04be8ff3f2f1
bernardo.448964 <bernardo.448964@ANNEX-PC28>
2026-08-25 19:44:28 +0800

    Add project files.

---
+++ 04.performance1_Bernardo/frmWordGuess.cs
@@ -0,0 +1,10 @@
+namespace _04.performance1_Bernardo
+{
+    public partial class frmWordGuess : Form
+    {
+        public frmWordGuess()
+        {
+            InitializeComponent();
+        }
+    }
+}
